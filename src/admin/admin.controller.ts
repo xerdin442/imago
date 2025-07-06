3b2f3fb9e90a40ad13c6ca73c4cb1b24757c5be9
xerdin442 <mudianthonio27@gmail.com>
2025-07-06 20:58:50 +0100

    updated logic for admin module

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -1,4 +1,137 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import { AdminAuthDTO, CreateAdminDTO } from './dto';
+import { SuperAdminGuard } from '../custom/guards/admin.guard';
+import { AdminService } from './admin.service';
+import logger from '@src/common/logger';
+import { Admin, Chat } from '@prisma/client';
 
 @Controller('admin')
-export class AdminController {}
+export class AdminController {
+  private readonly context: string = AdminController.name;
+
+  constructor(private readonly adminService: AdminService) {}
+
+  @Post('signup')
+  async signup(@Body() dto: AdminAuthDTO): Promise<{ message: string }> {
+    try {
+      await this.adminService.signup(dto);
+
+      logger.info(
+        `[${this.context}] Super Admin profile created by ${dto.email}.\n`,
+      );
+
+      return { message: 'Super Admin created successfully' };
+    } catch (error) {
+      logger.error(
+        `[${this.context}] An error occurred during super admin signup. Error: ${error.message}.\n`,
+      );
+
+      throw error;
+    }
+  }
+
+  @Post('login')
+  @HttpCode(HttpStatus.OK)
+  async login(@Body() dto: AdminAuthDTO): Promise<{ admin: Admin }> {
+    try {
+      const admin = await this.adminService.login(dto);
+
+      logger.info(
+        `[${this.context}] Admin profile login successful. Email: ${dto.email}.\n`,
+      );
+
+      return { admin };
+    } catch (error) {
+      logger.error(
+        `[${this.context}] An error occurred during admin login. Error: ${error.message}.\n`,
+      );
+
+      throw error;
+    }
+  }
+
+  @Get()
+  @UseGuards(SuperAdminGuard)
+  async getAllAdmins(): Promise<{ admins: Admin[] }> {
+    try {
+      return { admins: await this.adminService.getAllAdmins() };
+    } catch (error) {
+      logger.error(
+        `[${this.context}] An error occurred while retrieving profile details of sub admins. Error: ${error.message}.\n`,
+      );
+
+      throw error;
+    }
+  }
+
+  @Post('add')
+  @HttpCode(HttpStatus.OK)
+  @UseGuards(SuperAdminGuard)
+  async addAdmin(@Body() dto: CreateAdminDTO): Promise<{ message: string }> {
+    try {
+      await this.adminService.addAddmin(dto);
+
+      logger.info(
+        `[${this.context}] ${dto.email} has been added as a dispute resolution admin.\n`,
+      );
+
+      return { message: 'New admin added successfully' };
+    } catch (error) {
+      logger.error(
+        `[${this.context}] An error occurred while adding a new admin. Error: ${error.message}.\n`,
+      );
+
+      throw error;
+    }
+  }
+
+  @Post('remove/:adminId')
+  @HttpCode(HttpStatus.OK)
+  @UseGuards(SuperAdminGuard)
+  async removeAdmin(
+    @Param('adminId', ParseIntPipe) adminId: number,
+  ): Promise<{ message: string }> {
+    try {
+      const email = await this.adminService.removeAddmin(adminId);
+
+      logger.info(
+        `[${this.context}] ${email} has been removed as a dispute resolution admin.\n`,
+      );
+
+      return { message: 'Admin profile deleted successfully' };
+    } catch (error) {
+      logger.error(
+        `[${this.context}] An error occurred while deleting admin profile. Error: ${error.message}.\n`,
+      );
+
+      throw error;
+    }
+  }
+
+  @Get('disputes/:adminId')
+  async getDisputeChats(
+    @Param('adminId', ParseIntPipe) adminId: number,
+  ): Promise<{ chats: Chat[] }> {
+    try {
+      return {
+        chats: await this.adminService.getDisputeChats(adminId),
+      };
+    } catch (error) {
+      logger.error(
+        `[${this.context}] An error occurred while retrieving admin's dispute chats. Error: ${error.message}.\n`,
+      );
+
+      throw error;
+    }
+  }
+}
--- src/admin/admin.service.ts
+++ src/admin/admin.service.ts
@@ -1,4 +1,131 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { Admin, Chat } from '@prisma/client';
+import { randomUUID } from 'crypto';
+import { AdminAuthDTO, CreateAdminDTO } from './dto';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import { DbService } from '@src/db/db.service';
+import { sendEmail } from '@src/common/config/mail';
+import { Secrets } from '@src/common/secrets';
 
 @Injectable()
-export class AdminService {}
+export class AdminService {
+  constructor(private readonly prisma: DbService) {}
+
+  async signup(dto: AdminAuthDTO): Promise<void> {
+    try {
+      const admins = await this.prisma.admin.findMany();
+      if (admins.length === 1) {
+        throw new BadRequestException(
+          'Only one Super Admin profile can be created',
+        );
+      }
+
+      const hash = await argon.hash(dto.passcode);
+      await this.prisma.admin.create({
+        data: {
+          ...dto,
+          passcode: hash,
+          name: 'Super Admin',
+          category: 'OTHERS',
+          disputes: 0,
+        },
+      });
+
+      return;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async login(dto: AdminAuthDTO): Promise<Admin> {
+    try {
+      const admin = await this.prisma.admin.findUnique({
+        where: { email: dto.email },
+      });
+      // Check if admin is found with given email address
+      if (!admin) {
+        throw new BadRequestException('No admin found with that email address');
+      }
+
+      // Check if password is valid
+      const checkPassword = await argon.verify(admin.passcode, dto.passcode);
+      if (!checkPassword) {
+        throw new BadRequestException('Access denied. Invalid passcode');
+      }
+
+      admin.passcode = 'X-X-X';
+
+      return admin;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async getAllAdmins(): Promise<Admin[]> {
+    try {
+      return this.prisma.admin.findMany({
+        where: { NOT: { id: 1 } },
+      });
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async addAddmin(dto: CreateAdminDTO): Promise<void> {
+    try {
+      const passcode = randomUUID().split('-').slice(1, 4).join('-');
+      const hash = await argon.hash(passcode);
+
+      const admin = await this.prisma.admin.create({
+        data: {
+          ...dto,
+          disputes: 0,
+          passcode: hash,
+        },
+      });
+
+      // Send passcode to new admin
+      const subject = 'Login Details';
+      const content = `Welcome to the team! You're now a dispute resolution admin at ${Secrets.APP_NAME}. Your passcode is: ${passcode}.`;
+      await sendEmail(admin.email, subject, content);
+
+      return;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          const meta = error.meta as Record<string, any>;
+
+          throw new BadRequestException(
+            `This ${meta.target[0]} already exists. Please try again!`,
+          );
+        }
+      }
+
+      throw error;
+    }
+  }
+
+  async removeAddmin(adminId: number): Promise<string> {
+    try {
+      const admin = await this.prisma.admin.delete({
+        where: { id: adminId },
+      });
+
+      return admin.email;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async getDisputeChats(adminId: number): Promise<Chat[]> {
+    try {
+      return this.prisma.chat.findMany({
+        where: { adminId },
+        include: { messages: true },
+      });
+    } catch (error) {
+      throw error;
+    }
+  }
+}
