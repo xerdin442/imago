f11a07af68c94238c40f6457f94d18ef5b10538d
xerdin442 <mudianthonio27@gmail.com>
2025-08-20 01:31:32 +0100

    fixed websocket errors

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,6 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 import helmet from 'helmet';
 import * as cookieParser from 'cookie-parser';
 import { Secrets } from './common/secrets';
+import { IoAdapter } from '@nestjs/platform-socket.io';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,7 @@ async function bootstrap() {
   app.enableCors();
   app.use(helmet());
   app.use(cookieParser());
+  app.useWebSocketAdapter(new IoAdapter(app));
   app.setGlobalPrefix('/api');
 
   app.useGlobalPipes(
